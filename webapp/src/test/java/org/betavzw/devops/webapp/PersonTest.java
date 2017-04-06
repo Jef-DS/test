c741108ad94ff2df2fd9d1b466ea6606709e100c
Jef De Smedt <jef.desmedt@betavzw.org>
2017-04-06 08:16:12 +0200

    person class

---
+++ webapp/src/test/java/org/betavzw/devops/webapp/PersonTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.betavzw.devops.webapp;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jef
+ */
+public class PersonTest {
+    
+    public PersonTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testPersonSetName(){
+        String expected = "Joske";
+        Person p = new Person();
+        p.setName(expected);
+        String result = p.getName();
+        assertEquals(expected, result);
+    }
+}
